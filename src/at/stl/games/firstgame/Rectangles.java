f926e865c45102b93fc71a33568d5ae6b36bc2a9
Lenifie <leoniesophie.stecher@gmx.at>
2023-02-27 11:54:39 +0100

    Rectangle speed

--- src/at/stl/games/firstgame/Rectangles.java
+++ src/at/stl/games/firstgame/Rectangles.java
@@ -7,8 +7,9 @@ import org.newdawn.slick.tests.AnimationTest;
 import java.awt.*;
 
 public class Rectangles extends BasicGame {
-    private int x;
-    private int y;
+    private float x;
+    private float y;
+    private float speed;
 
 
     public Rectangles(String title) {
@@ -18,17 +19,14 @@ public class Rectangles extends BasicGame {
     @Override
     public void init(GameContainer gameContainer) throws SlickException {
         this.x = 100;
+        this.speed = 10.0f;
     }
 
     @Override
-    public void update(GameContainer gameContainer, int i) throws SlickException {
-        this.x++;
-        this.y++;
+    public void update(GameContainer gameContainer, int delta) throws SlickException {
+       this.x += (float)delta/speed;
 
-        if(this.x>800){
-            this.x=0;
-            this.y=0;
-        }
+
 
     }
 
