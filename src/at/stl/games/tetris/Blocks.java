35b748c18d9e1ba3abc6eafeb8ac26d272952a4b
Lenifie <leoniesophie.stecher@gmx.at>
2023-04-17 15:22:13 +0200

    11 tutorial finished does not go

--- src/at/stl/games/tetris/Blocks.java
+++ src/at/stl/games/tetris/Blocks.java
@@ -1,6 +1,8 @@
 package at.stl.games.tetris;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -29,10 +31,10 @@ public class Blocks extends JPanel {
             },
             {
                     //O
-                    {new Point(0,0),new Point(0,1),new Point(01,0),new Point(1,1)},
-                    {new Point(0,0),new Point(0,1),new Point(01,0),new Point(1,1)},
-                    {new Point(0,0),new Point(0,1),new Point(01,0),new Point(1,1)},
-                    {new Point(0,0),new Point(0,1),new Point(01,0),new Point(1,1)},
+                    {new Point(0,0),new Point(0,1),new Point(1,0),new Point(1,1)},
+                    {new Point(0,0),new Point(0,1),new Point(1,0),new Point(1,1)},
+                    {new Point(0,0),new Point(0,1),new Point(1,0),new Point(1,1)},
+                    {new Point(0,0),new Point(0,1),new Point(1,0),new Point(1,1)},
             },
     };
 
@@ -48,11 +50,11 @@ public class Blocks extends JPanel {
         well=new Color[12][24];
         for(int i=0;i<12;i++){
             for(int j = 0; j<23;j++){
-                if(i==0)||i==11||i=22){
-                    well[i][j]=Color.pink;
-                }else{
+                //if(i==0||11==i||i=22){
+                 //   well[i][j]=Color.pink;
+                //}else{
                     well[i][j]=Color.black;
-                }
+                //}
             }
         }
         newPiece();
@@ -150,9 +152,79 @@ public class Blocks extends JPanel {
             case 4:
                 score+=800;
                 break;
-
         }
     }
+    private void drawPiece(Graphics graphics){
+        graphics.setColor(myColor[currentPiece]);
+        for (Point p:myPoint[currentPiece][rotation]) {
+            graphics.fillRect((p.x+pt.x)*26,(pt.y+p.y)*26,25,25);
+        }
+    }
+    public void paintComponent(Graphics graphics){
+        graphics.fillRect(0,0,26*12,26*23);
+        for (int i = 0; i < 23; i++) {
+            for (int j = 0; j < 23; j++) {
+                graphics.setColor(well[i][j]);
+                graphics.fillRect(26*i,26*j,25,25);
+            }
+        }
+        graphics.setColor(Color.WHITE);
+        graphics.drawString("Score is: "+score, 19*12, 25);
+        drawPiece(graphics);
+    }
 
+    public static void main(String[] args) {
 
+        JFrame f = new JFrame("Blocks");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setSize(12*26+10,26*23+25);
+        f.setVisible(true);
+
+        final Blocks game=new Blocks();
+        game.init();
+        f.add(game);
+
+        f.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                switch (e.getKeyCode()){
+                    case KeyEvent.VK_UP:
+                        game.rotate(-1);
+                        break;
+                    case KeyEvent.VK_DOWN:
+                        game.rotate(+1);
+                        break;
+                    case KeyEvent.VK_LEFT:
+                        game.move(-1);
+                        break;
+                    case KeyEvent.VK_SPACE:
+                        game.drop();
+                        game.score+=1;
+                        break;
+
+                }
+            }
+        });
+        new Thread(() -> {
+            while (true){
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                game.drop();
+            }
+        }).start();
+    }
 }
+
